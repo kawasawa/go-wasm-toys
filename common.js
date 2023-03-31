598023b6f21e5f7c05a13b44a2f79e56b754ebf0
kawasawa <kawasawa@users.noreply.github.com>
2023-03-31 15:04:54 +0000

    deploy: e803dad1c016955d0ef82fa283ca32a7c1b1e843

---
+++ common.js
@@ -0,0 +1,84 @@
+// -----------------------------------------------------------------------------
+// メソッド
+// -----------------------------------------------------------------------------
+
+const goRun = () => {
+    const go = new Go();
+    WebAssembly.instantiateStreaming(fetch("./main.wasm"), go.importObject).then((source) => go.run(source.instance));
+}
+
+const validateResponse = (response) => {
+    if (response === null || !("error" in response)) return true;
+    alert(response.error);
+    return false;
+}
+
+const refreshResultTextArea = (resultTextAreaName, response) => {
+    if (response === null || !("count" in response)) return;
+
+    // レスポンスをマッピング
+    const textArea = document.getElementById(resultTextAreaName)
+    textArea.innerHTML = "";
+    for (let i = 0; i < response.count; i++) {
+        if (0 < i) textArea.innerHTML += '\n'
+        textArea.innerHTML += response[`value${i}`];
+    }
+
+    // テキストに応じてサイズを調整
+    if (textArea.style.height !== "auto") textArea.style.height = "auto";
+    if (textArea.innerHTML.indexOf('\n') !== -1) textArea.style.height = `${textArea.scrollHeight}px`;
+
+    // テキストを全選択
+    textArea.select();
+}
+
+const refreshResultInput = (resultInputFixedName, response) => {
+    if (response === null || !("count" in response)) return;
+
+    for (let i = 0; i < response.count; i++) {
+        // レスポンスをマッピング
+        const input = document.getElementById(`${resultInputFixedName}${i}`)
+        input.value = response[`value${i}`];
+
+        // 先頭要素を全選択
+        if (i === 0) input.select();
+    }
+}
+
+// -----------------------------------------------------------------------------
+// イベントハンドラ
+// -----------------------------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+    // 共通要素を読み込む
+    const directoryExists = (url) => {
+        const request = new XMLHttpRequest();
+        request.open('HEAD', url, false);
+        request.send();
+        return request.status !== 404;
+    }
+    const load = (elementId, path, after = false) => {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState !== 4 || request.status !== 200) return;
+            const element = document.getElementById(elementId);
+            element.innerHTML = after ? element.innerHTML + request.responseText : request.responseText + element.innerHTML;
+        }
+        request.open("GET", path, true);
+        request.send(null);
+    }
+    const commonDir = directoryExists("../common") ? "../common" : "./common";
+    load("layouts.header", `${commonDir}/header.html`);
+    load("layouts.footer", `${commonDir}/footer.html`, true);
+
+    // メタデータをマッピング
+    const intervalId = setInterval(() => {
+        const title = document.getElementById("title");
+        const description = document.getElementById("description");
+        if (title && description) {
+            clearInterval(intervalId);
+            title.innerText = document.title;
+            description.innerText = document.getElementsByName('description').item(0).content;
+        }
+    }, 100);
+});
